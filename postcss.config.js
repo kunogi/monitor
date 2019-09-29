1067f4c2cf8a67dfc25f33cf6899e85358126efa
kunogi <kunogi@hotmail.com>
2019-09-29 10:48:32 +0800

    删除无用

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            browsers: ['last 2 version', 'Android >= 4.0', 'ios >= 6']
-        }),
-        require('cssnano')
-    ]
-};
